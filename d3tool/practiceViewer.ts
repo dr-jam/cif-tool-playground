bb3ec20989470036d0a9b0935040162929925ec6
Josh McCoy <jam@american.edu>
2015-08-24 10:34:05 -0400

    Removed dependency on dist directory.

--- d3tool/practiceViewer.ts
+++ d3tool/practiceViewer.ts
@@ -1,11 +1,22 @@
 /// <reference path="typings/d3/d3.d.ts" />
 
 module CiFViz {
-  export class practiceViewer {
+  export class PracticeViewer {
 
+    private _baseSVG: d3.Selection<any>;
 
-    constructor(container: any) {
+    constructor() {
         console.log(d3.version);
+
+        //get the base SVG
+        this._baseSVG = d3.select("practiceViewerBase");
+        this._baseSVG.style("background-color", "#555555");
+        this._baseSVG.style("width", "600px");
+        this._baseSVG.style("height", "480px");
+    }
+
+    public render(data: any[]) {
+      
     }
 
   }
